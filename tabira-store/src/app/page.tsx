9fdea3184c56523ade3411ab93806d1688c90e31
Otávio Cipriano <mirllamsa@gmail.com>
2023-09-05 19:15:41 -0300

    Search bar

--- tabira-store/src/app/page.tsx
+++ tabira-store/src/app/page.tsx
@@ -1,9 +1,7 @@
 // Home.js (ou qualquer outro nome de arquivo que você está usando)
 import React from 'react';
 import Navbar from '../../Components/Navbar';
-import styles from "../../Styles/Page.module.css"; 
-import mockItems from '../../public/mock.data';
-import ProductLinkButton from '../../Components/ProductLinkButton';
+
 
 function Page() {
   return (
